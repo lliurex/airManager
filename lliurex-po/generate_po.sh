f6b15f87ba7f58f988bc54f7f7c11f2d146c7e5b
juanma1980 <juanma1980@gmail.com>
2020-03-09 11:34:42 +0100

    fix conflicts

---
+++ lliurex-po/generate_po.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MANAGER_PYTHON_FILES="../air-manager/src/*py"
+INSTALLER_PYTHON_FILES="../air-installer/src/*py" 
+UI_FILES="../air-manager/src/stacks/*py"
+
+mkdir -p air-manager/ air-installer/
+
+xgettext $UI_FILES $MANAGER_PYTHON_FILES -o air-manager/air-manager.pot
+
+xgettext $INSTALLER_PYTHON_FILES -o air-installer/air-installer.pot
+
